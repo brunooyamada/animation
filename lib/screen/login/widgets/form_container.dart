599c79c19bfb8109859e9498b85d4d1d417458d1
Bruno Oyamada <bruno.oyamada@hotmail.com>
2026-03-09 18:05:25 -0300

    Criando Tela de Login

--- lib/screen/login/widgets/form_container.dart
+++ lib/screen/login/widgets/form_container.dart
@@ -5,6 +5,11 @@ class FormContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(margin: EdgeInsets.symmetric(horizontal: 20));
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Form(child: Column(children: [
+
+      ],)),
+    );
   }
 }
